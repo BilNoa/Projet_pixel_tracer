c3310f1649e57e0dafe4e1394f8c717d9de2d904
Jihan <miouni499@gmail.com>
2026-04-08 09:26:47 +0200

    doc doxy

---
+++ doc doxy/html/search/all_d.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['radus_0',['radus',['../structcercle.html#ae5b93467f676e42b16d2e0898c6d0f4a',1,'cercle']]],
+  ['read_5fexec_5fcommand_1',['read_exec_command',['../command_8c.html#ab576f5b835e80a0ef9ba46654a6295cb',1,'read_exec_command(Pixel_tracer_app *app):&#160;command.c'],['../command_8h.html#ab576f5b835e80a0ef9ba46654a6295cb',1,'read_exec_command(Pixel_tracer_app *app):&#160;command.c']]],
+  ['read_5ffrom_5fstdin_2',['read_from_stdin',['../command_8h.html#a3ee388bf58d7da98c66b98d3d2ee63c5',1,'read_from_stdin(Command *cmd):&#160;command.c'],['../command_8c.html#a3ee388bf58d7da98c66b98d3d2ee63c5',1,'read_from_stdin(Command *cmd):&#160;command.c']]],
+  ['rectangle_3',['rectangle',['../structrectangle.html',1,'rectangle'],['../shape_8h.html#a13bca493ce80dfe198a36f528166d363',1,'Rectangle:&#160;shape.h'],['../shape_8h.html#a82a00e336c65dad4cc04b65563b26eb5ae552ab0a96c0384a6e918e726b7f6102',1,'RECTANGLE:&#160;shape.h']]],
+  ['red_4',['RED',['../shape_8h.html#a37dbdc30935031c05304482e1be89d8faf80f9a890089d211842d59625e561f88',1,'shape.h']]],
+  ['red_5fcell_5',['RED_CELL',['../render_8h.html#afb357ddf7c2eb20e7781c00ff0d2941faea77bf50e57f655f938db02ab0e7ce1e',1,'render.h']]],
+  ['remove_5farea_5ffrom_5flist_6',['remove_area_from_list',['../area_8c.html#a3836b8e2e44c3a032fe7b6dc57d4369d',1,'remove_area_from_list(AreaList *area_list, Area *area):&#160;area.c'],['../area_8h.html#a3836b8e2e44c3a032fe7b6dc57d4369d',1,'remove_area_from_list(AreaList *area_list, Area *area):&#160;area.c']]],
+  ['remove_5flayer_5ffrom_5flist_7',['remove_layer_from_list',['../layers_8c.html#a68278e8226ecbaf6bbe27addfb9152b6',1,'remove_layer_from_list(LayersList *layer_list, Layer *layer):&#160;layers.c'],['../layers_8h.html#a68278e8226ecbaf6bbe27addfb9152b6',1,'remove_layer_from_list(LayersList *layer_list, Layer *layer):&#160;layers.c']]],
+  ['remove_5fpixel_5fshape_8',['remove_pixel_shape',['../pixel_8h.html#aa885a1c0aceaa86e24b2822756553f14',1,'remove_pixel_shape(list *pixel_lst):&#160;pixel.c'],['../pixel_8c.html#aa885a1c0aceaa86e24b2822756553f14',1,'remove_pixel_shape(list *pixel_lst):&#160;pixel.c']]],
+  ['remove_5fshape_5fto_5ffrom_9',['remove_shape_to_from',['../layers_8c.html#aca54ff31c800dfa70eb0ff3019b9f05c',1,'remove_shape_to_from(Layer *layer, Shape *shape):&#160;layers.c'],['../layers_8h.html#aca54ff31c800dfa70eb0ff3019b9f05c',1,'remove_shape_to_from(Layer *layer, Shape *shape):&#160;layers.c']]],
+  ['render_2ec_10',['render.c',['../render_8c.html',1,'']]],
+  ['render_2eh_11',['render.h',['../render_8h.html',1,'']]],
+  ['rotation_12',['rotation',['../structshape.html#a3c69a753c9ae4b2aa7ac7c82de29c290',1,'shape']]]
+];
